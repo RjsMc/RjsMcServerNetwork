1292cf42001af53987c7a0bacdd3b158fe851ef5
RjsMc <rjsuwanto@gmail.com>
2025-10-19 15:31:12 -0400

    Added serveral servers

---
+++ RjsMcNetwork/start.sh
@@ -0,0 +1,1 @@
+java -Xms2G -Xmx2G -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -XX:MaxInlineLevel=15 -jar velocity*.jar nogui
